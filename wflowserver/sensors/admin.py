14a83eaa71763099e2c11862e3961de7fabfab2e
andreaarig <arigliano.andrea33@gmail.com>
2023-03-13 12:08:35 +0100

    Add sensor models admin view

--- wflowserver/sensors/admin.py
+++ wflowserver/sensors/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Sensor, SensorData, SensorTypeDefinition
 
-# Register your models here.
+
+class SendorAdmin(admin.ModelAdmin):
+    list_display = ('device_id', 'active', 'sensor_id', 'sensor_type')
+
+
+class SensorDataAdmin(admin.ModelAdmin):
+    list_display = ('key', 'device', 'created')
+
+
+class SensorTypeDefinitionAdmin(admin.ModelAdmin):
+    list_display = ('data_id', 'sensor_id', 'start_timestamp',
+                    'end_timestamp', 'values')
+
+
+admin.site.register(Sensor, SendorAdmin)
+admin.site.register(SensorData, SensorDataAdmin)
+admin.site.register(SensorTypeDefinition, SensorTypeDefinitionAdmin)
